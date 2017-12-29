0d010567a6168f3073ed4f9ce0fb2ad3004132c5
gespiton <1044750905@qq.com>
2017-12-29 11:02:40 +0800

    add controller test

--- src/main/java/xmu/crms/service/TopicService.java
+++ src/main/java/xmu/crms/service/TopicService.java
@@ -44,9 +44,9 @@ public interface TopicService {
      *
      * @param topicId 要删除的topic的topicId
      * @throws IllegalArgumentException Id格式错误时抛出
-     * @exception TopicNotFoundException 未找到该话题
+     * @throws TopicNotFoundException   未找到该话题
      */
-    void deleteTopicByTopicId(BigInteger topicId) throws IllegalArgumentException,TopicNotFoundException;
+    void deleteTopicByTopicId(BigInteger topicId) throws IllegalArgumentException, TopicNotFoundException;
 
 
     /**
@@ -97,19 +97,21 @@ public interface TopicService {
     /**
      * 按话题id和小组id获取讨论课小组选题信息（包括该小组该话题展示成绩）
      * <p>按话题id和小组id获取讨论课小组选题信息（包括该小组该话题展示成绩）<br>
+     *
      * @param topicId 话题id
      * @param groupId 组id
      * @return seminarGroupTopic 讨论课小组选题信息
-     * @exception IllegalArgumentException topicId或groupId格式错误
+     * @throws IllegalArgumentException topicId或groupId格式错误
      */
     SeminarGroupTopic getSeminarGroupTopicById(BigInteger topicId, BigInteger groupId) throws IllegalArgumentException;
 
     /**
      * 根据小组id获取该小组该堂讨论课所有选题信息
      * <p>根据小组id获取该小组该堂讨论课所有选题信息<br>
+     *
      * @param groupId
      * @return list 该小组该堂讨论课选题列表
-     * @exception IllegalArgumentException groupId格式错误
+     * @throws IllegalArgumentException groupId格式错误
      */
     List<SeminarGroupTopic> listSeminarGroupTopicByGroupId(BigInteger groupId) throws IllegalArgumentException;
 
--- src/test/java/xmu/crms/service/GradeServiceTest.java
+++ src/test/java/xmu/crms/service/GradeServiceTest.java
@@ -1,6 +1,7 @@
 package xmu.crms.service;
 
 import static org.junit.Assert.assertNotNull;
+
 import java.math.BigInteger;
 import java.util.List;
 
@@ -23,91 +24,90 @@ import xmu.crms.exception.UserNotFoundException;
 import xmu.crms.service.ClassService;
 
 /**
- * 
  * @author yjj
  * @date 2017-12-27
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CourseManageApplication.class)
 public class GradeServiceTest {
-	@Autowired
-	private GradeService gradeService;
+    @Autowired
+    private GradeService gradeService;
 
-	@Test
-	public void deleteStudentScoreGroupByTopicId() {
-		try {
-			gradeService.deleteStudentScoreGroupByTopicId(new BigInteger("1"));
-			gradeService.deleteStudentScoreGroupByTopicId(null);
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void deleteStudentScoreGroupByTopicId() {
+        try {
+            gradeService.deleteStudentScoreGroupByTopicId(new BigInteger("1"));
+            gradeService.deleteStudentScoreGroupByTopicId(null);
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void getSeminarGroupBySeminarGroupId() {
-		try {
-			SeminarGroup seminarGroup = gradeService.getSeminarGroupBySeminarGroupId(new BigInteger("1"));
-			System.err.println(seminarGroup);
-			seminarGroup = gradeService.getSeminarGroupBySeminarGroupId(null);
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (GroupNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void listSeminarGradeByCourseId() {
-		try {
-			List<SeminarGroup> seminarGroup = gradeService.listSeminarGradeByCourseId(new BigInteger("3"),new BigInteger("1"));
-			System.err.println(seminarGroup);
-			seminarGroup = gradeService.listSeminarGradeByCourseId(null,null);
-			
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} 
-	}
-	
-	@Test
-	public void insertGroupGradeByUserId() {
-		try {
-			gradeService.insertGroupGradeByUserId(new BigInteger("1"),new BigInteger("2"),new BigInteger("1"),new BigInteger("6"));
-			gradeService.insertGroupGradeByUserId(null,null,null,null);
-			
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} 
-	}
-	
-	@Test
-	public void updateGroupByGroupId() {
-		try {
-			gradeService.updateGroupByGroupId(new BigInteger("1"),new BigInteger("2"));
-			gradeService.updateGroupByGroupId(null,null);
-			
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (GroupNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	}
-	
-	@Test
-	public void countGroupGradeBySeminarId() {
-		try {
-			gradeService.countGroupGradeBySeminarId(new BigInteger("1"));
-			
-		} catch (IllegalArgumentException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} 
-	}
+    @Test
+    public void getSeminarGroupBySeminarGroupId() {
+        try {
+            SeminarGroup seminarGroup = gradeService.getSeminarGroupBySeminarGroupId(new BigInteger("1"));
+            System.err.println(seminarGroup);
+            seminarGroup = gradeService.getSeminarGroupBySeminarGroupId(null);
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        } catch (GroupNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void listSeminarGradeByCourseId() {
+        try {
+            List<SeminarGroup> seminarGroup = gradeService.listSeminarGradeByCourseId(new BigInteger("3"), new BigInteger("1"));
+            System.err.println(seminarGroup);
+            seminarGroup = gradeService.listSeminarGradeByCourseId(null, null);
+
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void insertGroupGradeByUserId() {
+        try {
+            gradeService.insertGroupGradeByUserId(new BigInteger("1"), new BigInteger("2"), new BigInteger("1"), new BigInteger("6"));
+            gradeService.insertGroupGradeByUserId(null, null, null, null);
+
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void updateGroupByGroupId() {
+        try {
+            gradeService.updateGroupByGroupId(new BigInteger("1"), new BigInteger("2"));
+            gradeService.updateGroupByGroupId(null, null);
+
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        } catch (GroupNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void countGroupGradeBySeminarId() {
+        try {
+            gradeService.countGroupGradeBySeminarId(new BigInteger("1"));
+
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
 
 }
