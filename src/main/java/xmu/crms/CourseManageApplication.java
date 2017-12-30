72c12de2e96cbbfb181943c10304d5289c3bcdc8
pocketstars <22320142201076@stu.xmu.edu.cn>
2017-12-30 10:31:32 +0800

    recover application

---
+++ src/main/java/xmu/crms/CourseManageApplication.java
@@ -0,0 +1,16 @@
+package xmu.crms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@MapperScan(basePackages = { "xmu.crms.mapper", "xmu.crms.dao" })
+@SpringBootApplication
+public class CourseManageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManageApplication.class, args);
+	}
+
+}
