60f60eda671265c9621e01fdc1b1da1e449b6415
pocketstars <22320142201076@stu.xmu.edu.cn>
2018-01-02 21:30:37 +0800

    fix some problem

--- src/main/java/xmu/crms/web/controller/UploadController.java
+++ src/main/java/xmu/crms/web/controller/UploadController.java
@@ -1,25 +1,15 @@
 package xmu.crms.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import xmu.crms.service.UploadService;
-import xmu.crms.web.VO.UploadAvatarVO;
-
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
